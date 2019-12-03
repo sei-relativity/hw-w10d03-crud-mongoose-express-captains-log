034f9cbe3cb4be4fbe8a9e7afb908975315401ec
almarrim <mosalmarri@gmail.com>
2019-12-03 21:15:48 +0300

    solution

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const logRoutes = require('./config/routes/log_routes')
 
+// Create Express App
 const app = express();
 
+// Set PORT
 const port = 3000
 
 app.listen(port, () => {
@@ -12,3 +15,11 @@ const mongooseBaseName = "captains-log"
 mongoose.connect(`mongodb://localhost:27017/${mongooseBaseName}-development`, { useNewUrlParser: true }, () => {
     console.log("Connected to MongoDB")
 });
+
+
+/**** Middleware ****/
+// Add bodyParser middleware to convert it to json
+app.use(express.json())
+
+/***** ROUTES *****/
+app.use(logRoutes)
